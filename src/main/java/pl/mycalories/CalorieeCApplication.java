450e6bdf9ef4f29b29976a098945705d33f3897b
BartoszDabek <bartek50600@wp.pl>
2017-09-02 21:25:36 +0200

    add data jpa dependency

--- src/main/java/pl/mycalories/CalorieeCApplication.java
+++ src/main/java/pl/mycalories/CalorieeCApplication.java
@@ -1,4 +1,4 @@
-package pl.devpress.CalorieeC;
+package pl.mycalories;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pl/mycalories/CalorieeCApplicationTests.java
+++ src/test/java/pl/mycalories/CalorieeCApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.devpress.CalorieeC;
+package pl.mycalories;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
