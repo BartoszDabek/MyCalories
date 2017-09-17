6bb3bee297a0d026f909d9addfbf1b44016926d3
BartoszDabek <bartek50600@wp.pl>
2017-09-17 16:54:19 +0200

    when product is deleted, productMeal set 'product_id' field to null

--- src/main/java/pl/mycalories/service/impl/ProductServiceImpl.java
+++ src/main/java/pl/mycalories/service/impl/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import pl.mycalories.model.Product;
+import pl.mycalories.model.ProductMeal;
 import pl.mycalories.service.ProductService;
 
 @Service
@@ -12,5 +13,13 @@ public class ProductServiceImpl extends AbstractServiceImpl<Product, Long> imple
     @Autowired
     public ProductServiceImpl(JpaRepository<Product, Long> repository) {
         super(repository);
+    }
+
+    @Override
+    public void delete(Product product) {
+        for(ProductMeal p: product.getProductMeals()) {
+            p.setProduct(null);
+        }
+        super.delete(product);
     }
 }
