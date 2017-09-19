bcfc3126d564e0b293a1c3fdc242947b6ce65dc0
BartoszDabek <bartek50600@wp.pl>
2017-09-19 10:19:03 +0200

    add new test cases

--- src/test/java/pl/mycalories/service/MealServiceImplTest.java
+++ src/test/java/pl/mycalories/service/MealServiceImplTest.java
@@ -12,8 +12,12 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import pl.mycalories.CalorieeCApplication;
 import pl.mycalories.dao.MealDao;
 import pl.mycalories.model.Meal;
+import pl.mycalories.model.NutritionalValues;
+import pl.mycalories.model.Product;
+import pl.mycalories.model.ProductMeal;
 import pl.mycalories.service.impl.MealServiceImpl;
 
+import java.util.HashSet;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -65,6 +69,42 @@ public class MealServiceImplTest {
         doNothing().when(mealDao).delete(meal);
         mealService.delete(meal);
         verify(mealDao, times(1)).delete(meal);
+    }
+
+    @Test
+    public void should_set_NutritionalValues_of_meal_when_save_is_called() {
+        NutritionalValues expectedNutritionalValues = new NutritionalValues(1000, 100, 100, 100);
+
+        Product product = new Product();
+        product.setNutritionalValues(new NutritionalValues(100, 10, 10, 10));
+
+        ProductMeal productMeal = new ProductMeal();
+        productMeal.setAmount(10);
+        productMeal.setProduct(product);
+
+        Meal meal = new Meal();
+        meal.setProductMeals(new HashSet<>());
+        meal.getProductMeals().add(productMeal);
+
+        when(mealDao.save(meal)).thenReturn(meal);
+        Meal savedMeal = mealService.save(meal);
+        Assert.assertEquals(expectedNutritionalValues, savedMeal.getNutritionalValues());
+    }
+
+    @Test
+    public void should_remove_related_ProductMeals_when_delete_is_salled() {
+        Meal meal = new Meal();
+        ProductMeal productMeal = new ProductMeal();
+        ProductMeal productMeal1 = new ProductMeal();
+        productMeal.setId(1L);
+        productMeal1.setId(2L);
+        meal.setProductMeals(new HashSet<>());
+        meal.getProductMeals().add(productMeal);
+        meal.getProductMeals().add(productMeal1);
+
+        mealService.delete(meal);
+
+        Assert.assertTrue(meal.getProductMeals().isEmpty());
     }
 
 }
---
+++ src/test/java/pl/mycalories/service/ProductServiceImplTest.java
@@ -0,0 +1,129 @@
+package pl.mycalories.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import pl.mycalories.CalorieeCApplication;
+import pl.mycalories.dao.ProductDao;
+import pl.mycalories.model.Product;
+import pl.mycalories.model.ProductMeal;
+import pl.mycalories.service.impl.ProductServiceImpl;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CalorieeCApplication.class)
+@WebAppConfiguration
+public class ProductServiceImplTest {
+
+    @Autowired
+    private ProductService productService;
+    @Mock
+    private ProductDao productDaoMock;
+    @Mock
+    private Product productMock;
+    @Mock
+    private List<Product> productsMock;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        productService = new ProductServiceImpl(productDaoMock);
+    }
+
+    @Test
+    public void should_Return_List_Of_Products_When_getAll_Is_Called() {
+        when(productDaoMock.findAll()).thenReturn(productsMock);
+        List<Product> retriviedProducts = productService.getAll();
+        Assert.assertThat(retriviedProducts, is(equalTo(productsMock)));
+    }
+
+    @Test
+    public void should_Return_Product_When_findById_Is_Called() {
+        when(productDaoMock.findOne(5L)).thenReturn(productMock);
+        Product retriviedProduct = productService.findById(5L);
+        Assert.assertThat(retriviedProduct, is(equalTo(productMock)));
+    }
+
+    @Test
+    public void should_Return_Product_When_Save_Is_Called() {
+        when(productDaoMock.save(productMock)).thenReturn(productMock);
+        Product savedProduct = productService.save(productMock);
+        Assert.assertThat(savedProduct, is(equalTo(productMock)));
+    }
+
+    @Test
+    public void should_Call_Delete_Method_On_productDaoMock_When_Delete_Is_Called() {
+        doNothing().when(productDaoMock).delete(productMock);
+        productService.delete(productMock);
+        verify(productDaoMock, times(1)).delete(productMock);
+    }
+
+    @Test
+    public void should_Set_Null_On_ProductMeal_When_Delete_Is_Called() {
+        ProductMeal productMeal = new ProductMeal();
+        Product product = new Product();
+        product.setProductMeals(new HashSet<>());
+        product.getProductMeals().add(productMeal);
+        productMeal.setProduct(product);
+
+        when(productDaoMock.save(product)).thenReturn(product);
+        Product newProduct = productService.save(product);
+
+        productService.delete(newProduct);
+        Assert.assertNull(productMeal.getProduct());
+    }
+
+    @Test
+    public void should_Set_Null_On_ProductMeals_When_Delete_Is_Called() {
+        ProductMeal productMeal1 = new ProductMeal();
+        ProductMeal productMeal2 = new ProductMeal();
+        productMeal1.setId(1L);
+        productMeal2.setId(2L);
+
+        Product product = new Product();
+        product.setProductMeals(new HashSet<>());
+        product.getProductMeals().add(productMeal1);
+        product.getProductMeals().add(productMeal2);
+
+        productMeal1.setProduct(product);
+        productMeal2.setProduct(product);
+
+        when(productDaoMock.save(product)).thenReturn(product);
+        Product newProduct = productService.save(product);
+
+        productService.delete(newProduct);
+        verify(productDaoMock, times(1)).delete(newProduct);
+        Assert.assertNull(productMeal1.getProduct());
+        Assert.assertNull(productMeal2.getProduct());
+    }
+
+    @Test
+    public void should_Not_Set_Null_On_ProductMeal_When_Delete_Is_Called() {
+        ProductMeal productMeal = new ProductMeal();
+        Product product = new Product();
+        product.setProductMeals(new HashSet<>());
+        product.getProductMeals().add(new ProductMeal());
+        productMeal.setProduct(productMock);
+
+        when(productDaoMock.save(product)).thenReturn(product);
+        Product newProduct = productService.save(product);
+
+
+        productService.delete(newProduct);
+        verify(productDaoMock, times(1)).delete(newProduct);
+        Assert.assertNotNull(productMeal.getProduct());
+    }
+}
