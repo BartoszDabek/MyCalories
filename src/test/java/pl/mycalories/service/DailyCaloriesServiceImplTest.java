b36c331e33f46bddf1538189763426ea777d1e0c
BartoszDabek <bartek50600@wp.pl>
2017-09-20 16:14:08 +0200

    add new test cases for dailyCalories class + delete unused annotation in productServiceTest

---
+++ src/test/java/pl/mycalories/service/DailyCaloriesServiceImplTest.java
@@ -0,0 +1,92 @@
+package pl.mycalories.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import pl.mycalories.CalorieeCApplication;
+import pl.mycalories.dao.DailyCaloriesDao;
+import pl.mycalories.model.*;
+import pl.mycalories.service.impl.DailyCaloriesServiceImpl;
+
+import java.util.Date;
+import java.util.HashSet;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CalorieeCApplication.class)
+@WebAppConfiguration
+public class DailyCaloriesServiceImplTest {
+
+    private DailyCaloriesService dailyCaloriesService;
+    @Mock
+    private DailyCaloriesDao dailyCaloriesDao;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        dailyCaloriesService = new DailyCaloriesServiceImpl(dailyCaloriesDao);
+    }
+
+    @Test
+    public void should_Set_NutritionalValues_When_Save_Is_Called() {
+        NutritionalValues expectedNutritionalValues = new NutritionalValues(2000, 200, 200, 200);
+        int expectedCalories = 2000;
+
+        Product product = new Product();
+        product.setNutritionalValues(new NutritionalValues(100, 10, 10, 10));
+
+        ProductMeal productMeal = new ProductMeal();
+        productMeal.setAmount(10);
+        productMeal.setProduct(product);
+
+        Meal meal = new Meal();
+        meal.setId(1L);
+        meal.setProductMeals(new HashSet<>());
+        meal.getProductMeals().add(productMeal);
+
+        Meal meal1 = new Meal();
+        meal1.setId(2L);
+        meal1.setProductMeals(new HashSet<>());
+        meal1.getProductMeals().add(productMeal);
+
+        DailyCalories dailyCalories = new DailyCalories();
+        dailyCalories.setMeals(new HashSet<>());
+        dailyCalories.getMeals().add(meal);
+        dailyCalories.getMeals().add(meal1);
+
+        when(dailyCaloriesDao.save(dailyCalories)).thenReturn(dailyCalories);
+        DailyCalories savedDailyCalories = dailyCaloriesService.save(dailyCalories);
+        Assert.assertEquals(expectedNutritionalValues, savedDailyCalories.getNutritionalValues());
+        Assert.assertEquals(expectedCalories, savedDailyCalories.getNutritionalValues().getCalories());
+    }
+
+    @Test
+    public void should_Set_newDate_When_Save_Is_Called() {
+        DailyCalories dailyCalories = new DailyCalories();
+
+        when(dailyCaloriesDao.save(dailyCalories)).thenReturn(dailyCalories);
+        DailyCalories savedDailyCalories = dailyCaloriesService.save(dailyCalories);
+        Assert.assertNotNull(savedDailyCalories.getDate());
+    }
+
+    @Test
+    public void should_Not_Set_newDate_When_Save_Is_Called_Because_Its_Update_Request() {
+        long timeInMiliSecondsTo2015_12_31 = 1451516400000L;
+        Date expectedDate = new Date(timeInMiliSecondsTo2015_12_31);
+        DailyCalories dailyCalories = new DailyCalories();
+        dailyCalories.setDate(expectedDate);
+
+        when(dailyCaloriesDao.save(dailyCalories)).thenReturn(dailyCalories);
+        DailyCalories savedDailyCalories = dailyCaloriesService.save(dailyCalories);
+        Assert.assertEquals(expectedDate, savedDailyCalories.getDate());
+    }
+
+
+}
--- src/test/java/pl/mycalories/service/ProductServiceImplTest.java
+++ src/test/java/pl/mycalories/service/ProductServiceImplTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -28,7 +27,6 @@ import static org.mockito.Mockito.*;
 @WebAppConfiguration
 public class ProductServiceImplTest {
 
-    @Autowired
     private ProductService productService;
     @Mock
     private ProductDao productDaoMock;
