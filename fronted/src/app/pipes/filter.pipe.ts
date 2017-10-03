10cbab7498573bc14956b27aed4a501afb91d217
BartoszDabek <bartek50600@wp.pl>
2017-10-03 11:02:41 +0200

    add filter pipe

---
+++ fronted/src/app/pipes/filter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(values: any, args?: any): any {
+    if (args === undefined) return values;
+    return values.filter(function(value) {
+      return value.name.toLowerCase().includes(args.toLowerCase());
+    })
+  }
+
+}
