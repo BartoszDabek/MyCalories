1070ea5a30bf0a01aae796d0b8a7be382d7976b8
BartoszDabek <bartek50600@wp.pl>
2017-10-05 11:17:04 +0200

    add nameInterface (to convert name inputs)

--- src/main/java/pl/mycalories/model/Category.java
+++ src/main/java/pl/mycalories/model/Category.java
@@ -3,18 +3,21 @@ package pl.mycalories.model;
 import javax.persistence.Entity;
 import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Entity
 @SequenceGenerator(name = "idgen", sequenceName = "category_id_seq", allocationSize = 1)
-public class Category extends AbstractModel {
+public class Category extends AbstractModel implements NameInterface, Serializable {
 
     @NotNull
     private String name;
 
+    @Override
     public String getName() {
         return name;
     }
 
+    @Override
     public void setName(String name) {
         this.name = name;
     }
---
+++ src/main/java/pl/mycalories/model/NameInterface.java
@@ -0,0 +1,8 @@
+package pl.mycalories.model;
+
+public interface NameInterface {
+    String name = null;
+
+    String getName();
+    void setName(String name);
+}
