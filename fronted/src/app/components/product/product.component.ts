fa406fbe9b2958cbb3b6442219dc890035b24203
BartoszDabek <bartek50600@wp.pl>
2017-10-06 07:49:38 +0200

    implement sorting possibility + add capitalizing first letter of product/category name

--- fronted/src/app/components/product/product.component.ts
+++ fronted/src/app/components/product/product.component.ts
@@ -13,9 +13,33 @@ import { ProductInterface } from '../../interfaces/product';
 })
 export class ProductComponent implements OnInit {
   products: ProductInterface[];
-  
-  constructor(private productService: ProductService ) {
-    
+  sortName: string;
+  reverseSort: boolean;
+  product: ProductInterface;
+
+  constructor(private productService: ProductService ) { }
+
+  ngOnInit() {
+    this.productService.getProducts()
+      .subscribe((response) => {
+        this.products = response;
+        for(let i=0; i<response.length; i++) {
+          this.products[i].name = this.capitalizeFirstLetterAndLowerCaseOthers(this.products[i].name);
+          this.products[i].category.name = this.capitalizeFirstLetterAndLowerCaseOthers(this.products[i].category.name);
+        }
+      });
+  }
+
+  capitalizeFirstLetterAndLowerCaseOthers(name: string) {
+    return name = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  }
+
+  setSortBy(name: string) {
+    this.sortName = name;
+  }
+
+  reverseSorting() {
+    this.reverseSort = !this.reverseSort;
   }
 
   search = (text$: Observable<string>) =>
@@ -24,14 +48,6 @@ export class ProductComponent implements OnInit {
     .map(term => term.length < 2 ? []
       : this.products.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
 
-formatter = (x: {name: string}) => x.name;
-
-  
-  ngOnInit() {
-    this.productService.getProducts()
-      .subscribe((response) => {
-        this.products = response;
-      });
-  }
+  formatter = (x: {name: string}) => x.name;
 
 }
